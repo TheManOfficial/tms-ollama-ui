6d1025588f6b885063fd3fc858a4369f184eb66d
The Man <themanofficial@forver.org>
2024-01-06 19:37:18 -0800

    Remove weird makefile start method, switching to start.sh instead.

--- start.sh
+++ start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo "Opening browser... (You may need to refresh the page)"
-sleep 1
 
-make && xdg-open http://localhost:8000/
+# Function to stop the server
+stop_server() {
+    echo "   Stopping server..."
+    kill -- -$$
+    exit 0
+}
+
+trap stop_server SIGINT
+
+echo "Opening browser... (You may need to refresh the page)"
+sleep 2
+
+python3 -m http.server --bind 127.0.0.1 3000 &
+xdg-open http://localhost:3000/
+
+wait
+
